cfae01282f352f72c9ab01c623c9499e8b50f27e
Roland Bayor <rbayor16@gmail.com>
2022-02-09 15:57:48 +0000

    made complex report queries

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -39,5 +39,7 @@ export class ReportsController {
   }
 
   @Get()
-  getEstimate(@Query() query: GetEstimateDto) {}
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createEstimate(query);
+  }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 import { Repository } from 'typeorm';
-import { CreateReportDto } from './dtos/report.dtos';
+import { CreateReportDto, GetEstimateDto } from './dtos/report.dtos';
 import { Report } from './report.entity';
 
 @Injectable()
@@ -22,5 +22,28 @@ export class ReportsService {
     report.approved = approved;
 
     return this.repo.save(report);
+  }
+
+  async createEstimate({
+    make,
+    model,
+    lng,
+    lat,
+    year,
+    mileage,
+  }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make= :make', { make })
+      .andWhere('model= :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -5 AND 5', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
   }
 }
