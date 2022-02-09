871bd54d77f5f1bd84d83a762f735770449537b9
Roland Bayor <rbayor16@gmail.com>
2022-02-09 15:39:15 +0000

    added current user as middleware

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,21 +1,25 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '../guards/auth.guard';
 import {
   ApproveReportDto,
   CreateReportDto,
+  GetEstimateDto,
   ReportDto,
 } from './dtos/report.dtos';
 import { ReportsService } from './reports.service';
 import { CurrentUser } from '../users/decorators/current_user.decorator';
 import { User } from '../users/user.entity';
 import { Serialize } from '../interceptors/serialize.interceptor';
+import { AdminGuard } from '../guards/admin.guard';
 
 @Controller('reports')
 export class ReportsController {
@@ -29,7 +33,11 @@ export class ReportsController {
   }
 
   @Patch('/:id')
+  @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {}
 }
